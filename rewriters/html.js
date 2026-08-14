e2f9507c8116c5b06a42dcb9fcaf09166d043b6a
ItsMeh1 <omarabuelfoutouh@gmail.com>
2026-08-13 19:13:43 -0700

    [core] update rewriter

--- rewriters/html.js
+++ rewriters/html.js
@@ -1,21 +1,21 @@
 export function rewriteHtml(html, pageUrl, proxyPrefix) {
     const base = new URL(pageUrl);
     
-    // Rewrite absolute URLs
+    // Rewrite absolute URLs in attributes
     html = html.replace(
-        /(\s(?:href|src|action|poster|data-src|data-href|data-url|data-icon|content)=["'])(https?:\/\/[^"']+)(["'])/gi,
+        /(\s(?:href|src|action|poster|data-src|data-href|data-url|content)=["'])(https?:\/\/[^"']+)(["'])/gi,
         (m, pre, url, suf) => `${pre}${proxyPrefix}/${url}${suf}`
     );
     
     // Rewrite root-relative URLs
     html = html.replace(
-        /(\s(?:href|src|action|poster|data-src|data-href|data-url|data-icon|content)=["'])(\/[^"']+)(["'])/gi,
+        /(\s(?:href|src|action|poster|data-src|data-href|data-url|content)=["'])(\/[^"']+)(["'])/gi,
         (m, pre, path, suf) => `${pre}${proxyPrefix}/${base.origin}${path}${suf}`
     );
     
     // Rewrite protocol-relative URLs
     html = html.replace(
-        /(\s(?:href|src|action|poster|data-src|data-href|data-url|data-icon|content)=["'])(\/\/[^"']+)(["'])/gi,
+        /(\s(?:href|src|action|poster|data-src|data-href|data-url|content)=["'])(\/\/[^"']+)(["'])/gi,
         (m, pre, url, suf) => `${pre}${proxyPrefix}/https:${url}${suf}`
     );
     
@@ -36,77 +36,73 @@ export function rewriteHtml(html, pageUrl, proxyPrefix) {
     html = html.replace(/<meta[^>]*http-equiv=["']Content-Security-Policy["'][^>]*>/gi, '');
     
     // Inject proxy bootstrap
-    const injection = `<script data-flashproxy-inject>
+    const injection = `<script data-flashproxy>
 (function(){
-    const __pp = '${proxyPrefix}';
-    const __origin = '${base.origin}';
-    const __page = '${pageUrl}';
+    const __pp='${proxyPrefix}';
+    const __page='${pageUrl}';
+    const __origin='${base.origin}';
+    window.__flashproxy_page=__page;
     
-    const __origFetch = window.fetch;
-    window.fetch = function(u, opts) {
-        if (typeof u === 'string') {
-            if (u.startsWith('http')) u = __pp + '/' + u;
-            else if (u.startsWith('/')) u = __pp + '/' + __origin + u;
-        } else if (u instanceof Request) {
-            const url = u.url;
-            const newUrl = url.startsWith('http') ? __pp + '/' + url 
-                : url.startsWith('/') ? __pp + '/' + __origin + url : url;
-            u = new Request(newUrl, u);
+    const _f=window.fetch;
+    window.fetch=function(u,o){
+        if(typeof u==='string'){
+            if(u.startsWith('http'))u=__pp+'/'+u;
+            else if(u.startsWith('/'))u=__pp+'/'+__origin+u;
+        }else if(u instanceof Request){
+            const r=u.url;
+            u=new Request(r.startsWith('http')?__pp+'/'+r:r.startsWith('/')?__pp+'/'+__origin+r:r,u);
         }
-        return __origFetch(u, opts);
+        return _f(u,o);
     };
     
-    const __origXHR = window.XMLHttpRequest;
-    window.XMLHttpRequest = function() {
-        const xhr = new __origXHR();
-        const __open = xhr.open;
-        xhr.open = function(m, u, a, user, pw) {
-            if (typeof u === 'string') {
-                if (u.startsWith('http')) u = __pp + '/' + u;
-                else if (u.startsWith('/')) u = __pp + '/' + __origin + u;
+    const _x=window.XMLHttpRequest;
+    window.XMLHttpRequest=function(){
+        const x=new _x(),o=x.open;
+        x.open=function(m,u,a,user,pw){
+            if(typeof u==='string'){
+                if(u.startsWith('http'))u=__pp+'/'+u;
+                else if(u.startsWith('/'))u=__pp+'/'+__origin+u;
             }
-            return __open.call(xhr, m, u, a, user, pw);
-    };
-        return xhr;
+            return o.call(x,m,u,a,user,pw);
+        };
+        return x;
     };
     
-    const __origWS = window.WebSocket;
-    window.WebSocket = function(url, protocols) {
-        if (typeof url === 'string') {
-            if (url.startsWith('ws://') || url.startsWith('wss://')) {
-                url = (location.protocol === 'https:' ? 'wss:' : 'ws:') + '//' + location.host + '/wisp/' + url;
-            }
+    const _w=window.WebSocket;
+    window.WebSocket=function(url,p){
+        if(typeof url==='string'&&(url.startsWith('ws://')||url.startsWith('wss://'))){
+            url=(location.protocol==='https:'?'wss:':'ws:')+'//'+location.host+'/wisp/'+url;
         }
-        return new __origWS(url, protocols);
+        return new _w(url,p);
     };
     
-    const __origES = window.EventSource;
-    window.EventSource = function(url, opts) {
-        if (typeof url === 'string') {
-            if (url.startsWith('http')) url = __pp + '/' + url;
-            else if (url.startsWith('/')) url = __pp + '/' + __origin + url;
+    const _e=window.EventSource;
+    window.EventSource=function(url,o){
+        if(typeof url==='string'){
+            if(url.startsWith('http'))url=__pp+'/'+url;
+            else if(url.startsWith('/'))url=__pp+'/'+__origin+url;
         }
-        return new __origES(url, opts);
+        return new _e(url,o);
     };
     
-    const __loc = new URL(__page);
-    Object.defineProperty(window, 'location', {
-        get: () => __loc,
-        set: (v) => {
-            if (typeof v === 'string') {
-                if (v.startsWith('http')) window.top.postMessage({__fp_nav: v}, '*');
-                else if (v.startsWith('/')) window.top.postMessage({__fp_nav: __origin + v}, '*');
-                else __loc.href = v;
+    const _loc=new URL(__page);
+    Object.defineProperty(window,'location',{
+        get:()=>_loc,
+        set:(v)=>{
+            if(typeof v==='string'){
+                if(v.startsWith('http'))window.top.postMessage({__fp_nav:v},'*');
+                else if(v.startsWith('/'))window.top.postMessage({__fp_nav:__origin+v},'*');
+                else _loc.href=v;
             }
         }
     });
     
-    Object.defineProperty(document, 'location', {
-        get: () => __loc,
-        set: (v) => { window.location = v; }
+    Object.defineProperty(document,'location',{
+        get:()=>_loc,
+        set:(v)=>{window.location=v;}
     });
     
-    console.log('[FlashProxy] Runtime active on', __page);
+    console.log('[FlashProxy] Injected');
 })();
 </script>`;
     
