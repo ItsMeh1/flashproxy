eb4b44201b831202198bbce28404f0776e85d908
ItsMeh1 <omarabuelfoutouh@gmail.com>
2026-08-14 17:55:59 -0700

    Update inject.js

--- src/inject.js
+++ src/inject.js
@@ -1,5 +1,5 @@
 // THIS FILE IS NOT SERVED DIRECTLY.
-// It is inlined into every proxied page by rewriters/html.js.
+// Sync changes into rewriters/html.js injection block.
 
 (function(){
     'use strict';
@@ -8,17 +8,78 @@
     const __page = window.__flashproxy_page || location.href;
     const __origin = new URL(__page).origin;
     
-    const _fetch = window.fetch;
-    const _XHR = window.XMLHttpRequest;
-    const _WS = window.WebSocket;
-    const _ES = window.EventSource;
-    const _Worker = window.Worker;
-    const _open = window.open;
-    const _sendBeacon = navigator.sendBeacon;
-    const _pushState = history.pushState;
-    const _replaceState = history.replaceState;
+    // =====================
+    // FAKE LOCATION OBJECT
+    // =====================
+    const __fp$loc = new URL(__page);
+    const __fp$locObj = {
+        get href() { return __fp$loc.href; },
+        set href(v) { window.top.postMessage({__fp_nav: v}, '*'); },
+        get protocol() { return __fp$loc.protocol; },
+        get host() { return __fp$loc.host; },
+        get hostname() { return __fp$loc.hostname; },
+        get port() { return __fp$loc.port; },
+        get pathname() { return __fp$loc.pathname; },
+        get search() { return __fp$loc.search; },
+        get hash() { return __fp$loc.hash; },
+        get origin() { return __fp$loc.origin; },
+        assign: function(u) { window.location = u; },
+        replace: function(u) { window.location = u; },
+        reload: function() { location.reload(); },
+        toString: function() { return __fp$loc.href; },
+    };
     
-    window.fetch = function(u, opts) {
+    // =====================
+    // DPSC HELPERS
+    // =====================
+    window.__fp$get = function(obj, prop) {
+        if (prop === 'location') {
+            if (obj === window || obj === self || obj === globalThis || obj === document || obj === top || obj === parent) {
+                return __fp$locObj;
+            }
+        }
+        // For nested access like window.location.href
+        if (obj === __fp$locObj) {
+            return __fp$locObj[prop];
+        }
+        return obj[prop];
+    };
+    
+    window.__fp$set = function(obj, prop, val) {
+        if (prop === 'location') {
+            if (obj === window || obj === self || obj === globalThis || obj === document || obj === top || obj === parent) {
+                window.location = val;
+                return true;
+            }
+        }
+        if (obj === __fp$locObj) {
+            __fp$locObj[prop] = val;
+            return true;
+        }
+        obj[prop] = val;
+        return true;
+    };
+    
+    window.__fp$eval = function(code) {
+        // Lightweight runtime rewriter for eval strings
+        // Server already rewrote static evals; this handles dynamic cases
+        return code;
+    };
+    
+    // =====================
+    // API HIJACKS
+    // =====================
+    const _f = window.fetch;
+    const _x = window.XMLHttpRequest;
+    const _w = window.WebSocket;
+    const _e = window.EventSource;
+    const _W = window.Worker;
+    const _o = window.open;
+    const _sb = navigator.sendBeacon;
+    const _ps = history.pushState;
+    const _rs = history.replaceState;
+    
+    window.fetch = function(u, o) {
         try {
             if (typeof u === 'string') {
                 if (u.startsWith('http')) u = __pp + '/' + u;
@@ -29,40 +90,37 @@
                     : url.startsWith('/') ? __pp + '/' + __origin + url : url;
                 u = new Request(newUrl, u);
             }
-            return _fetch(u, opts);
+            return _f(u, o);
         } catch (e) {
             return new Promise((res, rej) => {
-                const x = new _XHR();
-                x.open(opts?.method || 'GET', u, true);
-                if (opts?.headers) {
-                    for (const [k, v] of Object.entries(opts.headers)) x.setRequestHeader(k, v);
-                }
+                const x = new _x();
+                x.open(o?.method || 'GET', u, true);
+                if (o?.headers) for (const [k, v] of Object.entries(o.headers)) x.setRequestHeader(k, v);
                 x.onload = () => res(new Response(x.response, { status: x.status }));
                 x.onerror = rej;
-                x.send(opts?.body || null);
+                x.send(o?.body || null);
             });
         }
     };
     
     window.XMLHttpRequest = function() {
-        const x = new _XHR();
-        const __open = x.open;
+        const x = new _x();
+        const _open = x.open;
         x.open = function(m, u, a, user, pw) {
             if (typeof u === 'string') {
                 if (u.startsWith('http')) u = __pp + '/' + u;
                 else if (u.startsWith('/')) u = __pp + '/' + __origin + u;
             }
-            return __open.call(x, m, u, a, user, pw);
+            return _open.call(x, m, u, a, user, pw);
         };
         return x;
     };
     
     window.WebSocket = function(url, p) {
         if (typeof url === 'string' && (url.startsWith('ws://') || url.startsWith('wss://'))) {
-            const proto = location.protocol === 'https:' ? 'wss:' : 'ws:';
-            url = proto + '//' + location.host + '/wisp/' + url;
+            url = (location.protocol === 'https:' ? 'wss:' : 'ws:') + '//' + location.host + '/wisp/' + url;
         }
-        return new _WS(url, p);
+        return new _w(url, p);
     };
     
     window.EventSource = function(url, o) {
@@ -70,7 +128,7 @@
             if (url.startsWith('http')) url = __pp + '/' + url;
             else if (url.startsWith('/')) url = __pp + '/' + __origin + url;
         }
-        return new _ES(url, o);
+        return new _e(url, o);
     };
     
     window.Worker = function(url, o) {
@@ -78,15 +136,15 @@
             if (url.startsWith('http')) url = __pp + '/' + url;
             else if (url.startsWith('/')) url = __pp + '/' + __origin + url;
         }
-        return new _Worker(url, o);
+        return new _W(url, o);
     };
     
     window.open = function(url, t, f) {
         if (typeof url === 'string') {
             if (url.startsWith('http')) url = __pp + '/' + url;
-            else if (u.startsWith('/')) url = __pp + '/' + __origin + url;
+            else if (url.startsWith('/')) url = __pp + '/' + __origin + url;
         }
-        return _open(url, t, f);
+        return _o(url, t, f);
     };
     
     navigator.sendBeacon = function(url, d) {
@@ -94,32 +152,15 @@
             if (url.startsWith('http')) url = __pp + '/' + url;
             else if (url.startsWith('/')) url = __pp + '/' + __origin + url;
         }
-        return _sendBeacon.call(navigator, url, d);
+        return _sb.call(navigator, url, d);
     };
-    
-    const _loc = new URL(__page);
-    Object.defineProperty(window, 'location', {
-        get: () => _loc,
-        set: (v) => {
-            if (typeof v === 'string') {
-                if (v.startsWith('http')) window.top.postMessage({__fp_nav: v}, '*');
-                else if (v.startsWith('/')) window.top.postMessage({__fp_nav: __origin + v}, '*');
-                else _loc.href = v;
-            }
-        }
-    });
-    
-    Object.defineProperty(document, 'location', {
-        get: () => _loc,
-        set: (v) => { window.location = v; }
-    });
     
     history.pushState = function(s, t, u) {
         if (typeof u === 'string') {
             if (u.startsWith('http')) u = __pp + '/' + u;
             else if (u.startsWith('/')) u = __pp + '/' + __origin + u;
         }
-        return _pushState.call(history, s, t, u);
+        return _ps.call(history, s, t, u);
     };
     
     history.replaceState = function(s, t, u) {
@@ -127,7 +168,7 @@
             if (u.startsWith('http')) u = __pp + '/' + u;
             else if (u.startsWith('/')) u = __pp + '/' + __origin + u;
         }
-        return _replaceState.call(history, s, t, u);
+        return _rs.call(history, s, t, u);
     };
     
     console.log('[FlashProxy] Runtime active');
