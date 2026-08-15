72157331f68c3945fd6e586b7de7aa810cb75994
ItsMeh1 <omarabuelfoutouh@gmail.com>
2026-08-14 18:00:28 -0700

    Update html.js

--- rewriters/html.js
+++ rewriters/html.js
@@ -1,58 +1,3 @@
-import { parseDocument } from 'htmlparser2';
-import { render } from 'dom-serializer';
-import { Element, Text } from 'domhandler';
-import { removeElement } from 'domutils';
-
-const URL_ATTRS = ['href', 'src', 'action', 'poster', 'data-src', 'data-href', 'data-url', 'content'];
-
-function rewriteUrl(url, base, fpPrefix) {
-    if (!url || url.startsWith('data:') || url.startsWith('#') || url.startsWith('javascript:')) return url;
-    if (url.startsWith(fpPrefix)) return url;
-    if (url.startsWith('http://') || url.startsWith('https://')) return `${fpPrefix}/${url}`;
-    if (url.startsWith('//')) return `${fpPrefix}/https:${url}`;
-    if (url.startsWith('/')) return `${fpPrefix}/${base.origin}${url}`;
-    try {
-        return `${fpPrefix}/${new URL(url, base).href}`;
-    } catch {
-        return url;
-    }
-}
-
-export function rewriteHtml(html, pageUrl, fpPrefix) {
-    const base = new URL(pageUrl);
-    const doc = parseDocument(html, { lowerCaseAttributeNames: false, decodeEntities: true });
-    
-    const elements = [];
-    function walk(node) {
-        if (node.type === 'tag') {
-            elements.push(node);
-            if (node.children) for (const child of node.children) walk(child);
-        }
-    }
-    for (const child of doc.children) walk(child);
-    
-    for (const el of elements) {
-        for (const attr of URL_ATTRS) {
-            if (el.attribs[attr]) {
-                el.attribs[attr] = rewriteUrl(el.attribs[attr], base, fpPrefix);
-            }
-        }
-        
-        if (el.name === 'base' && el.attribs.href) {
-            el.attribs.href = rewriteUrl(el.attribs.href, base, fpPrefix);
-        }
-        
-        if (el.name === 'link' && el.attribs.rel === 'manifest') {
-            removeElement(el);
-            continue;
-        }
-        
-        if (el.name === 'meta' && el.attribs['http-equiv']?.toLowerCase() === 'content-security-policy') {
-            removeElement(el);
-            continue;
-        }
-    }
-    
     const injection = new Element('script', { 'data-flashproxy': '' });
     const injectionText = new Text(`(function(){
 'use strict';
@@ -60,6 +5,11 @@ const __pp='${fpPrefix}';
 const __page='${pageUrl}';
 const __origin='${base.origin}';
 window.__flashproxy_page=__page;
+const __fp$loc=new URL(__page);
+const __fp$locObj={get href(){return __fp$loc.href;},set href(v){window.top.postMessage({__fp_nav:v},'*');},get protocol(){return __fp$loc.protocol;},get host(){return __fp$loc.host;},get hostname(){return __fp$loc.hostname;},get port(){return __fp$loc.port;},get pathname(){return __fp$loc.pathname;},get search(){return __fp$loc.search;},get hash(){return __fp$loc.hash;},get origin(){return __fp$loc.origin;},assign:function(u){window.location=u;},replace:function(u){window.location=u;},reload:function(){location.reload();},toString:function(){return __fp$loc.href;}};
+window.__fp$get=function(obj,prop){if(prop==='location'){if(obj===window||obj===self||obj===globalThis||obj===document||obj===top||obj===parent)return __fp$locObj;}if(obj===__fp$locObj)return __fp$locObj[prop];return obj[prop];};
+window.__fp$set=function(obj,prop,val){if(prop==='location'){if(obj===window||obj===self||obj===globalThis||obj===document||obj===top||obj===parent){window.location=val;return true;}}if(obj===__fp$locObj){__fp$locObj[prop]=val;return true;}obj[prop]=val;return true;};
+window.__fp$eval=function(code){return code;};
 const _f=window.fetch,_x=window.XMLHttpRequest,_w=window.WebSocket,_e=window.EventSource,_W=window.Worker,_o=window.open,_sb=navigator.sendBeacon,_ps=history.pushState,_rs=history.replaceState;
 window.fetch=function(u,o){try{if(typeof u==='string'){if(u.startsWith('http'))u=__pp+'/'+u;else if(u.startsWith('/'))u=__pp+'/'+__origin+u;}else if(u instanceof Request){const r=u.url;u=new Request(r.startsWith('http')?__pp+'/'+r:r.startsWith('/')?__pp+'/'+__origin+r:r,u);}return _f(u,o);}catch(e){return new Promise((res,rej)=>{const x=new _x();x.open(o?.method||'GET',u,true);if(o?.headers)for(const[k,v]of Object.entries(o.headers))x.setRequestHeader(k,v);x.onload=()=>res(new Response(x.response,{status:x.status}));x.onerror=rej;x.send(o?.body||null);});}};
 window.XMLHttpRequest=function(){const x=new _x(),op=x.open;x.open=function(m,u,a,user,pw){if(typeof u==='string'){if(u.startsWith('http'))u=__pp+'/'+u;else if(u.startsWith('/'))u=__pp+'/'+__origin+u;}return op.call(x,m,u,a,user,pw);};return x;};
@@ -68,30 +18,7 @@ window.EventSource=function(url,o){if(typeof url==='string'){if(url.startsWith('
 window.Worker=function(url,o){if(typeof url==='string'){if(url.startsWith('http'))url=__pp+'/'+url;else if(url.startsWith('/'))url=__pp+'/'+__origin+url;}return new _W(url,o);};
 window.open=function(url,t,f){if(typeof url==='string'){if(url.startsWith('http'))url=__pp+'/'+url;else if(url.startsWith('/'))url=__pp+'/'+__origin+url;}return _o(url,t,f);};
 navigator.sendBeacon=function(url,d){if(typeof url==='string'){if(url.startsWith('http'))url=__pp+'/'+url;else if(url.startsWith('/'))url=__pp+'/'+__origin+url;}return _sb.call(navigator,url,d);};
-const _loc=new URL(__page);
-Object.defineProperty(window,'location',{get:()=>_loc,set:(v)=>{if(typeof v==='string'){if(v.startsWith('http'))window.top.postMessage({__fp_nav:v},'*');else if(v.startsWith('/'))window.top.postMessage({__fp_nav:__origin+v},'*');else _loc.href=v;}}});
-Object.defineProperty(document,'location',{get:()=>_loc,set:(v)=>{window.location=v;}});
 history.pushState=function(s,t,u){if(typeof u==='string'){if(u.startsWith('http'))u=__pp+'/'+u;else if(u.startsWith('/'))u=__pp+'/'+__origin+u;}return _ps.call(history,s,t,u);};
 history.replaceState=function(s,t,u){if(typeof u==='string'){if(u.startsWith('http'))u=__pp+'/'+u;else if(u.startsWith('/'))u=__pp+'/'+__origin+u;}return _rs.call(history,s,t,u);};
 console.log('[FlashProxy] Injected');
 })();`);
-    injection.children = [injectionText];
-    injectionText.parent = injection;
-    
-    const head = elements.find(el => el.name === 'head');
-    if (head) {
-        head.children.unshift(injection);
-        injection.parent = head;
-    } else {
-        const htmlEl = elements.find(el => el.name === 'html');
-        if (htmlEl) {
-            htmlEl.children.unshift(injection);
-            injection.parent = htmlEl;
-        } else {
-            doc.children.unshift(injection);
-            injection.parent = doc;
-        }
-    }
-    
-    return render(doc, { decodeEntities: false });
-}
